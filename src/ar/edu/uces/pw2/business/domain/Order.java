2dfeb306c02a58d1972cd17eec95f29cd25d50ee
billgiberti <web2alumax@gmail.com>
2018-10-21 21:51:11 -0300

    orderDao orderController Order

--- src/ar/edu/uces/pw2/business/domain/Order.java
+++ src/ar/edu/uces/pw2/business/domain/Order.java
@@ -3,6 +3,7 @@ package ar.edu.uces.pw2.business.domain;
 import java.util.*;
 
 public class Order {
+	private int id;
 	private User user;
 	private List <Item> itemsList;
 	private String qr;
@@ -10,13 +11,20 @@ public class Order {
 	private Date date;
 	private OrderState orderState;
 	
-	public Order (User prmUser, List <Item> itemsList, String prmOrderType, Date prmDate){
+	public Order (User prmUser, String prmOrderType, Date prmDate){
+		// añadir items luego de instanciar Order o
+		//incorporar List <Item> en el constructor??
 		this.user=prmUser;
 		this.itemsList = new ArrayList<Item>();
 		this.orderType=prmOrderType;
 		this.date=prmDate;		 
 	}
-	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id=id;
+	}
 	public List<Item> getItemsList() {
 		return itemsList;
 	}
@@ -55,5 +63,27 @@ public class Order {
 	public void setOrderState(OrderState orderState) {
 		this.orderState = orderState;
 	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (int) (id ^ (id >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Order other = (Order) obj;
+		if (this.id != other.id)
+			return false;
+		return true;
+	}
+
 
 }
